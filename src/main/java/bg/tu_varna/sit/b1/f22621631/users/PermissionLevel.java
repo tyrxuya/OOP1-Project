01fb804606323dbf2a0cd9e58b9c82a5158e46d6
Ivan Georgiev <ivan2003444@gmail.com>
2024-03-10 17:42:09 +0200

    Added base classes for the users

---
+++ src/main/java/bg/tu_varna/sit/b1/f22621631/users/PermissionLevel.java
@@ -0,0 +1,16 @@
+package main.java.bg.tu_varna.sit.b1.f22621631.users;
+
+public enum PermissionLevel {
+    USER("User"),
+    ADMINISTRATOR("Admin");
+
+    private final String permissionLevel;
+
+    PermissionLevel(String permissionLevel) {
+        this.permissionLevel = permissionLevel;
+    }
+
+    public String getPermissionLevel() {
+        return permissionLevel;
+    }
+}
---
+++ src/main/java/bg/tu_varna/sit/b1/f22621631/users/User.java
@@ -0,0 +1,7 @@
+package main.java.bg.tu_varna.sit.b1.f22621631.users;
+
+public class User {
+    private String username;
+    private String password;
+    private PermissionLevel permissionLevel;
+}
